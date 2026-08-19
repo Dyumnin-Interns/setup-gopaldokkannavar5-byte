6dcee7e25bb928b9290e17b2272674ddc36be291
gopaldokkannavar5-byte <gopaldokkannavar5@gmail.com>
2026-08-19 22:40:12 +0530

    Implement test cases for dut signals
    Added test cases for signals a, b, and c in dut_test.

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,7 +1,13 @@
 import cocotb
 from cocotb.triggers import Timer
-
-
 @cocotb.test()
 async def dut_test(dut):
+    
+    a(0, 1, 0, 1)
+    b(0, 0, 1, 1)
+    c(0, 1, 1, 0)
+    for i in range(4):
+        dut.a.value=a[i]
+        dut.b.value=b[i]
+        
     assert 0, "Test Not Implemented Error"
